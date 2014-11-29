184c1ded3519bb8775870cbb8ae3441404d40b77
LMarynissen <lukas.marynissen@student.howest.be>
2014-11-29 20:29:35 +0100

    items worden opgehaald, kunnen verplaatst worden

--- js/view.js
+++ js/view.js
@@ -1,20 +1,98 @@
 (function(){
 
+      var teller = 0;
         
-       function init(){
-      
-          //GEWOON EEN KLEIN TESTJE. MAG GEDELETE WORDEN :D
-          var stickys = document.querySelectorAll('.stickyNote');
-          console.log(stickys);
-          [].forEach.call(stickys, function(sticky){
-            console.log(sticky.style.left);
-            sticky.style.left = 300+ "px";
-            console.log(sticky.style.left);
-            //sticky.addEventListener("mousedown", document.mousedownHandler.bind(this));
-          });
-        
-       }
+      function init(){
+
+          for(var i = 0; i < items.length; i++){
+             var sticky = new Sticky(
+             items[i].Color,
+             items[i].contentlink,
+             items[i].datum,
+             items[i].description,
+             items[i].extension,
+             items[i].id,
+             items[i].posX,
+             items[i].posY,
+             items[i].project_id,
+             items[i].title,
+             items[i].user_id
+             );
+             document.body.appendChild(sticky.el);
+          }
+      }
+
+      function Sticky(Color, contentlink, datum, description, extension, id, posX, posY, project_id, title, user_id) {
+           
+        //aanmaken HTML-element
+        this.el = document.createElement('div');
+        this.el.classList.add('stickyNote');
+         
+        var titleEl = document.createElement("h3");      
+        var t = document.createTextNode(title);       
+        titleEl.appendChild(t);                           
+        this.el.appendChild(titleEl);    
+
+        var descriptionEl = document.createElement("h4");       
+        var t = document.createTextNode(description);       
+        descriptionEl.appendChild(t);                         
+        this.el.appendChild(descriptionEl);    
+
+        var thumbnail = document.createElement("div");   
+        thumbnail.classList.add('stickyContent');
+        thumbnail.style.backgroundImage = 'url(images/' + contentlink + '_th.' + extension + ')';
+        this.el.appendChild(thumbnail);
+
+        var datumEl = document.createElement("p");   
+        datumEl.classList.add('stickyDate');    
+        var t = document.createTextNode("aangemaakt op " + datum);       
+        datumEl.appendChild(t);                     
+        this.el.appendChild(datumEl);
+
+        var creatorEl = document.createElement("p");   
+        creatorEl.classList.add('stickyCreator');    
+        var t = document.createTextNode("door " + user_id);       
+        creatorEl.appendChild(t);                     
+        this.el.appendChild(creatorEl);            
+
+        this.el.style.left = posX + "px";
+        this.el.style.top = posY + "px";
+
+        this.el.style.backgroundColor = Color;
           
+        //handlers binden
+        this._mouseDownHandler = this.mouseDownHandler.bind(this);
+        this._mouseMoveHandler = this.mouseMoveHandler.bind(this);
+        this._mouseUpHandler = this.mouseUpHandler.bind(this);
+
+        this.el.addEventListener('mousedown', this._mouseDownHandler);
+      }
+
+        Sticky.prototype.mouseDownHandler = function (event) {
+
+            this.offsetX = event.offsetX;
+            this.offsetY = event.offsetY;
+
+            window.addEventListener('mousemove', this._mouseMoveHandler);
+            window.addEventListener('mouseup', this._mouseUpHandler);
+
+            teller++;
+            this.el.style.zIndex = teller;
+          }
+
+          Sticky.prototype.mouseMoveHandler = function (event) {
+            console.log("mouseMoveHandler");
+            this.el.style.left = (event.x - this.offsetX) + 'px';
+            this.el.style.top = (event.y - this.offsetY )+ 'px';
+          }
+
+          Sticky.prototype.mouseUpHandler = function (event) {
+            console.log("mouseUpHandler");
+
+            window.removeEventListener('mousemove', this._mouseMoveHandler);
+            window.removeEventListener('mouseup', this._mouseUpHandler);
+          }
        
+
        init();
 })();
--- view/Projects/view.php
+++ view/Projects/view.php
@@ -10,9 +10,11 @@
   <div class="projectWindow">
 
   <?php if (!empty($items)) {
+
+
 		foreach ($items as $item ) {
 	    	//echo "<a href=\"index.php?page=detail&amp;id={$project['id']}\"> {$project['title']} </a>";
-	    	echo "  	
+	    /*	echo "  	
 	    		<div class=\"stickyNote\" posX=\"{$item['posX']}\" posY=\"{$item['posY']}\" style=\"left:{$item['posX']}px; top:{$item['posY']}px; \">
   					<h3 class=\"stickyTitle\">{$item['title']}</h3>
   					<h4 class=\"stickyTitle\">{$item['description']}</h4>
@@ -20,10 +22,15 @@
 					<button class=\"stickyDeleteButton\">Delete</button>
   				</div>	
   				";
+  		*/
 		}
 	} else {
 		echo "<p>No stickies placed yet</p>";
     } ?>
 
 </section>
+<script type="text/javascript">
+//MAAKT EEN JSON BESTAND VAN ALLE $ITEMS
+          var items = <?php echo json_encode($items) ?>;
+</script>
 <script src="js/view.js"></script>
